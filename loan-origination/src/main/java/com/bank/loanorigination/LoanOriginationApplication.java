f81d0787ba084e072187c542bc0e0d5a7b5a48ba
Manuel Martin <mmjukisz@gmail.com>
2025-06-19 12:09:54 +0200

    feat: add /ping health endpoint

---
+++ loan-origination/src/main/java/com/bank/loanorigination/LoanOriginationApplication.java
@@ -0,0 +1,13 @@
+package com.bank.loanorigination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanOriginationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanOriginationApplication.class, args);
+	}
+
+}
---
+++ loan-origination/src/test/java/com/bank/loanorigination/LoanOriginationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.loanorigination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanOriginationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
